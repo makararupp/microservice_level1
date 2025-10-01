c76826db584b4dcc2efe49f6e6d728358abcc5db
makaraDevIt <makarasam68@gmail.com>
2025-10-02 00:33:59 +0700

    Revert "config git private"
    This reverts commit a89bd13d43461c46f57ab17d2c72bec4805c013c.
    
    git push origin main

--- account/account/src/main/java/com/makara/account/AccountApplication.java
+++ account/account/src/main/java/com/makara/account/AccountApplication.java
@@ -2,9 +2,7 @@ package com.makara.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-@RefreshScope
 @SpringBootApplication
 public class AccountApplication {
 
